76985a9ba20678bb0b5ac3cbfd3f9ce0a690db08
zhaoxu1943 <463156880@qq.com>
2020-05-20 12:35:39 +0800

    2020.05.19 dubbo 注解配置学习

---
+++ dubbo-demo-annotation-provider-01/src/main/java/AnnotationProvider.java
@@ -0,0 +1,13 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author zhaoxu
+ * @className AnnotationProvider
+ * @projectName ConcentrationDubbo
+ * @date 2020/5/20 12:15
+ */
+public class AnnotationProvider {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext()
+    }
+}
